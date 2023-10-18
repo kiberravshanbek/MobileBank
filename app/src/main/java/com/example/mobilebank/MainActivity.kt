63726c464ffb6290ae70fb89535ce733a11b8657
Nurali <rasuljonovnurali04@gmail.com>
2023-10-18 14:15:59 +0500

    Rasuljonov2

--- app/src/main/java/com/example/mobilebank/MainActivity.kt
+++ app/src/main/java/com/example/mobilebank/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.mobilebank
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mobilebank.ui.SignInScreen
+import com.example.mobilebank.ui.SignUpScreen
 import com.example.mobilebank.ui.Splash
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.container,SignInScreen()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.container,SignUpScreen()).commit()
     }
 }
---
+++ app/src/main/java/com/example/mobilebank/ui/SignUpScreen.kt
@@ -0,0 +1,8 @@
+package com.example.mobilebank.ui
+
+import androidx.fragment.app.Fragment
+import com.example.mobilebank.R
+
+class SignUpScreen:Fragment(R.layout.sign_up_screen) {
+
+}
